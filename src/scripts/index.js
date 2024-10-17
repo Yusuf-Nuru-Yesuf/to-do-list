a10ed524e1ef8a0d48df5b099995d4ccf88f545b
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-10-17 15:34:50 +0800

    Add index fie to manage the scripts through module

---
+++ src/scripts/index.js
@@ -0,0 +1,11 @@
+import "../styles/styles.css";
+import "../styles/nav.css";
+import "../styles/display.css";
+import {DisplayController} from './UI.js';
+
+
+document.addEventListener("DOMContentLoaded", DisplayController.init);
+
+
+
+     
